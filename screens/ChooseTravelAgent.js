3458467519387aeec927e7a02ba7f2de51efafeb
adavize <adavize@thepoint.africa>
2020-06-18 08:04:33 +0100

    add agent card

--- screens/ChooseTravelAgent.js
+++ screens/ChooseTravelAgent.js
@@ -1,8 +1,56 @@
 import React from 'react';
-import {View, SafeAreaView, ScrollView, Text} from 'react-native';
+import {View, SafeAreaView, ScrollView, Text, Image} from 'react-native';
 import {styles} from '../assets/styles/styles';
 
 import ScreenTopNav from '../components/screenTopNav';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+const TravelAgentCard = () => {
+  return (
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        marginTop: 20,
+        padding: 20,
+        borderRadius: 20,
+      }}>
+      <View style={{width: '32%'}}>
+        <Image
+          source={require('../assets/images/destinations/The-great-mountain-of-fujiyama.png')}
+          alt="image"
+        />
+      </View>
+      <View
+        style={{
+          marginLeft: 20,
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          width: '68%',
+        }}>
+        <Text style={{fontSize: 20, fontWeight: '500', color: '#333'}}>
+          Barokah international tour & travel
+        </Text>
+
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Text style={{marginRight: 10, color: '#bbb'}}>$2500</Text>
+          <Text style={{marginRight: 10, color: '#FF6E6E', fontWeight: '500'}}>
+            $2500/day
+          </Text>
+          <View
+            style={{borderRadius: 5, backgroundColor: '#FF6E6E', padding: 5}}>
+            <Text
+              style={{
+                color: 'white',
+              }}>
+              12%
+            </Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const ChooseTravelAgent = ({navigation}) => {
   return (
@@ -15,10 +63,39 @@ const ChooseTravelAgent = ({navigation}) => {
               showEllipse={false}
               title="Choose a travel agent"
             />
-            <Text style={{paddingVertical: 10}}>ss</Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginVertical: 15,
+              }}>
+              <Text style={{paddingVertical: 10}}>
+                Showing 12 travel agents
+              </Text>
+              <Text
+                style={{
+                  borderColor: '#FF6E6E',
+                  color: '#FF6E6E',
+                  borderWidth: 1,
+                  borderRadius: 5,
+                  paddingHorizontal: 20,
+                  fontSize: 12,
+                  paddingVertical: 10,
+                }}>
+                Filter
+              </Text>
+            </View>
           </View>
           <View style={styles.container}>
-            <Text>hello</Text>
+            <TravelAgentCard />
+            <TravelAgentCard />
+            <TravelAgentCard />
+            <TravelAgentCard />
+            <TravelAgentCard />
+            <TravelAgentCard />
+            <TravelAgentCard />
+            <TravelAgentCard />
+            <TravelAgentCard />
           </View>
         </ScrollView>
       </SafeAreaView>
